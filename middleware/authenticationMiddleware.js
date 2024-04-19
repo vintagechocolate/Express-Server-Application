707f7b9b4ca590190eb049ca545b09c31edd9a16
vintagechocolate <faroukouedraogo3@gmail.com>
2024-04-19 12:13:45 -0400

    Implemeted

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -1,7 +1,26 @@
+const auth = require('basic-auth');
+
+
 function authenticationMiddleware(req, res, next) {
-    // Authentication logic here (e.g., check if user is logged in)
-    console.log("Authentication middleware");
-    next();
-  }
+    // Parse user from Authorization header 
+    let user = auth(req);
+    
+    if (!user || !user.name || !user.pass) {
+        // If user is not found or credentials are missing, send a 401 status
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        return res.status(401).send();
+    }
+  
+     // Here you would normally validate the username and password against your database:
+     
+    if (user.name === 'admin' && user.pass === 'secret') { 
+         // If credentials are valid, continue to next middleware
+        return next();
+    } else {
+        // If credentials are invalid, send a 401 status
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        return res.status(401).send();
+    }
+}
   
 module.exports = authenticationMiddleware;
